fc6927d33753ca2f4ec5f0855368058c3d71db59
kc0 <katiec2312@gmail.com>
2024-06-10 11:38:08 +0100

    Updated prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,22 +1,26 @@
 // 'use client'
+import getDomain from "@/app/lib/getDomain"
 
 async function getData() {
-    // // 1 - grab API endpoint
-    // const endpoint = "http://localhost:3000/api/posts";
-    // // response
-    // const res = await fetch(endpoint); //HTTP GET
+    // 1 - grab API endpoint
+    const domain = getDomain();
+    const endpoint = `${domain}/api/posts`;
+    // response
+    const res = await fetch(endpoint); //HTTP GET
 
-    // // if response is not OK, throw an error
-    // if (!res.ok) 
-    //     throw new Error("Failed to fetch data")
-    // // return response of json
-    // return res.json();
-    return {items: []};
+    // if response is not OK, throw an error
+    if (!res.ok) 
+        throw new Error("Failed to fetch data")
+    // return response of json
+    return res.json();
+    // return {items: []};
 }
 
 export default async function BlogPage() {
     const data = await getData();
     const items = data && data.items ? [...data.items] : []
+    console.log(items);
+    console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
     return <main>
         <h1>Hello World</h1>
         <p>Posts:</p>
