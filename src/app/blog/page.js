a5851376026adb22565dabe21ae787aee122bf5d
kc0 <katiec2312@gmail.com>
2024-06-10 10:43:44 +0100

    Removed fetch

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,17 +1,17 @@
 // 'use client'
 
 async function getData() {
-    // 1 - grab API endpoint
-    const endpoint = "http://localhost:3000/api/posts";
-    // response
-    const res = await fetch(endpoint); //HTTP GET
+    // // 1 - grab API endpoint
+    // const endpoint = "http://localhost:3000/api/posts";
+    // // response
+    // const res = await fetch(endpoint); //HTTP GET
 
-    // if response is not OK, throw an error
-    if (!res.ok) 
-        throw new Error("Failed to fetch data")
-    // return response of json
-    return res.json();
-    
+    // // if response is not OK, throw an error
+    // if (!res.ok) 
+    //     throw new Error("Failed to fetch data")
+    // // return response of json
+    // return res.json();
+    return {items: []};
 }
 
 export default async function BlogPage() {
