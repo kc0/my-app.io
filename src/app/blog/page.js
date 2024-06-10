45deed05995bfe1fb523958d87839cc7ce98da11
kc0 <katiec2312@gmail.com>
2024-06-10 14:34:28 +0100

    update fetch

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,35 +1,30 @@
 // 'use client'
 import getDomain from "@/app/lib/getDomain"
-
-//fetch caching options
-//force-cache, no-store, revalidate: n seconds
-
 async function getData() {
-    // 1 - grab API endpoint
-    const domain = getDomain();
-    const endpoint = `${domain}/api/posts`;
-    // response
-    const res = await fetch(endpoint); //HTTP GET
-
-    // if response is not OK, throw an error
-    if (!res.ok) 
+    // 1 endpoint - API?
+    const domain = getDomain()
+    const endpoint = `${domain}/api/posts` // -> third party api request??
+    const res = await fetch(endpoint) // HTTP GET
+    if (!res.ok) {
         throw new Error("Failed to fetch data")
+    }
 
     if (res.headers.get("content-type") !== "application/json") {
-        return {items: []};
+        return {items: []}
     }
-    return res.json();
+    return res.json()
 }
 
+
 export default async function BlogPage() {
-    const data = await getData();
+    const data = await getData()
     const items = data && data.items ? [...data.items] : []
-    console.log(items);
-    console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+    console.log(items)
+    console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
     return <main>
         <h1>Hello World</h1>
         <p>Posts:</p>
-        {items && items.map((item, idx) => {
+        {items && items.map((item, idx)=>{
             return <li key={`post-${idx}`}>{item.title}</li>
         })}
     </main>
